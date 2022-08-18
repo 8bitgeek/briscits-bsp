661e17100839ee4fd96a78c6c4dd14fb91c06e55
8bitgeek <mike@8bitgeek.net>
2022-08-18 01:17:16 -0400

    open7xii bsp wip

--- _src/_bsp/_chip/gd32/gd32vf103xb/_src.mk
+++ _src/_bsp/_chip/gd32/gd32vf103xb/_src.mk
@@ -1,20 +1,8 @@
 # gd32vf103xb
 
-SRC_CHIP=${SRC_ROOT}/_bsp/_chip/gd32/gd32vf103xb
-INC += -I $(SRC_CHIP)
-INC += -I $(SRC_CHIP)/../
+SRC_CHIP_GD32VF103XB=${SRC_ROOT}/_bsp/_chip/gd32/gd32vf103xb
+INC += -I $(SRC_CHIP_GD32VF103XB)
 
-SRCS_AS += $(SRC_CHIP)/vectors.S
-SRCS_AS += $(SRC_CHIP)/../boot.S
+SRCS_AS += $(SRC_CHIP_GD32VF103XB)/vectors.S
 
-SRCS_CC += $(SRC_CHIP)/../bss_init.c
-SRCS_CC += $(SRC_CHIP)/../gd32vf103_dbg.c
-SRCS_CC += $(SRC_CHIP)/../gd32vf103_fwdgt.c
-SRCS_CC += $(SRC_CHIP)/../gd32vf103_gpio.c
-SRCS_CC += $(SRC_CHIP)/../gd32vf103_rcu.c
-SRCS_CC += $(SRC_CHIP)/../gd32vf103_spi.c
-SRCS_CC += $(SRC_CHIP)/../gd32vf103_usart.c
-SRCS_CC += $(SRC_CHIP)/../n200_func.c
-SRCS_CC += $(SRC_CHIP)/../system_gd32vf103.c
-
-include ${SRC_ROOT}/briscits/cpu/riscv/BumbleBee/RV32IMAC/_src.mk
+include ${SRC_CHIP_GD32VF103XB}/../_src.mk
