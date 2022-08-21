d2ea972b2f76a4be5f21c52fb7f4564994808ad4
8bitgeek <mike@8bitgeek.net>
2022-08-20 22:43:28 -0400

    bug-o-matic target cloned from bluepill

---
+++ _src/_bsp/_board/bug-o-matic/_src.mk
@@ -0,0 +1,10 @@
+# bluepill
+
+SRC_BOARD=${SRC_ROOT}/_bsp/_board/${TARGET}
+INC += -I $(SRC_BOARD)
+
+SRCS_CC += $(SRC_BOARD)/brisc_board.c
+
+include ${SRC_ROOT}/_bsp/_chip/stm32/f1/stm32f103/_src.mk
+include ${SRC_BOARD}/_libopencm3.mk
+include ${SRC_ROOT}/b_gpio/_src.mk
---
+++ _src/_bsp/_board/bug-o-matic/brisc_board.c
@@ -0,0 +1,79 @@
+/******************************************************************************
+ ________  ________  ___  ________  ________  ___  _________  ________      
+|\   __  \|\   __  \|\  \|\   ____\|\   ____\|\  \|\___   ___\\   ____\     
+\ \  \|\ /\ \  \|\  \ \  \ \  \___|\ \  \___|\ \  \|___ \  \_\ \  \___|_    
+ \ \   __  \ \   _  _\ \  \ \_____  \ \  \    \ \  \   \ \  \ \ \_____  \   
+  \ \  \|\  \ \  \\  \\ \  \|____|\  \ \  \____\ \  \   \ \  \ \|____|\  \  
+   \ \_______\ \__\\ _\\ \__\____\_\  \ \_______\ \__\   \ \__\  ____\_\  \ 
+    \|_______|\|__|\|__|\|__|\_________\|_______|\|__|    \|__| |\_________\
+                            \|_________|                        \|_________|
+                            
+MIT License
+
+Copyright (c) 2022 Mike Sharkey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+******************************************************************************/
+#include <brisc_board.h>
+#include <brisc_thread.h>
+
+gpio_t  gpio_led0 = { GPIOC, GPIO13 };
+
+void board_init( void ) 
+{
+	/* Configure the system clock */
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+
+	/* Enables peripheral clocks */
+	rcc_periph_clock_enable(RCC_AFIO);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_GPIOD);
+	rcc_periph_clock_enable(RCC_DMA1);
+	rcc_periph_clock_enable(RCC_SRAM);
+	rcc_periph_clock_enable(RCC_CRC);
+	rcc_periph_clock_enable(RCC_USART1);
+	rcc_periph_clock_enable(RCC_USART2);
+	rcc_periph_clock_enable(RCC_WWDG);
+	rcc_periph_clock_enable(RCC_PWR);
+	rcc_periph_clock_enable(RCC_USB);
+	rcc_periph_clock_enable(RCC_TIM8);
+
+	/* Configur pins */
+	gpio_set_mode( GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13 );
+
+	/* Set initial gpio pin states */
+	b_gpio_set(&gpio_led0);
+
+	/* Configure the systick interrupt to generate systick interrupts at 1000 Hz */
+	systick_set_frequency(HZ, board_clkfreq());
+	systick_interrupt_enable();
+	systick_counter_enable();
+
+	/* Enable global CPU interrupts */
+	cpu_int_enable();
+}
+
+extern uint32_t board_clkfreq( void )
+{
+	/** @@NOTE Can we retrieve this from the rcc module? */
+    return 72000000;
+}
